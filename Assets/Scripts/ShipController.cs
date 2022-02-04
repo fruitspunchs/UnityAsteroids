332820103b4b7f29db68037ce70e4c5a998c8079
fruitspunchs <zargon234@gmail.com>
2022-02-04 23:01:08 +0800

    feat: add ship and movement

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2d.rotation += horizontal * -1;
+        lookDirection = transform.right;
+        Debug.Log(lookDirection);
+        rigidbody2d.position += lookDirection * vertical * Time.deltaTime;
+    }
+}
